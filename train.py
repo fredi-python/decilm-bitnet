130964edfc9a0c312399ed7644c60cf342c69fc1
fredithefish <83492589+fredi-python@users.noreply.github.com>
2024-03-30 00:13:31 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
-from transformers import (AutoTokenizer, AutoConfig, LlamaForCausalLM, DataCollatorForLanguageModeling, Trainer, TrainingArguments)
+from transformers import (AutoTokenizer, AutoConfig, DataCollatorForLanguageModeling, Trainer, TrainingArguments)
+from decilm.modeling_decilm import DeciLMForCausalLM
 from datasets import load_dataset
 from huggingface_hub import login
 import wandb
@@ -6,47 +7,16 @@ from utils import *
 
 ### Login
 # Wandb is for logging and is optional.
-hf_token = "<your_hf_token>"
-wb_token = "<your_wb_token>"
+hf_token = "huggingface token here"
+wb_token = "wandb token here"
 wandb.login(key=wb_token)
 login(token=hf_token)
-
-### Load and tokenize training data. Uncomment these lines to load and tokenize yourself.
-# data_source = "Skylion007/openwebtext"
-# data = load_dataset(data_source)
-# subset = load_dataset(data_source, split="train[:15%]")
-
-# context_length = 256
-# tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
-
-# def tokenize(element):
-#     outputs = tokenizer(
-#         element["text"],
-#         truncation=False,
-#         max_length=context_length,
-#         return_overflowing_tokens=True,
-#         return_length=True,
-#     )
-#     # Combine all tokens
-#     combined = []
-#     for tokenized_doc in outputs['input_ids']:
-#         combined += tokenized_doc + [tokenizer.eos_token_id]
-#     # Chunk
-#     input_batch = []
-#     for i in range(0, len(combined) - context_length, context_length):
-#         input_batch.append(combined[i:i+context_length])
-#     return {"input_ids": input_batch}
-
-# tokenized_data = subset.map(
-#     tokenize, batched=True, remove_columns=data["train"].column_names, 
-# )
-### End Load and tokenize training data
 
 ### Load pretokenized data (15% of openwebtext tokenized for ctx len of 256, ~1.5B tokens)
 # You can subset this even further if you want a smaller dataset.
 
 context_length = 256
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+tokenizer = AutoTokenizer.from_pretrained("Deci/DeciLM-7B")
 tokenized_data = load_dataset("xz56/openwebtext-tokenized-small")
 
 total_tokens = tokenized_data['train'].num_rows * context_length
@@ -54,7 +24,7 @@ print(f"Training on {total_tokens:_} tokens")
 
 ### Adjust llama config to make the model tiny
 config = AutoConfig.from_pretrained(
-    "meta-llama/Llama-2-7b-hf",
+    "Deci/DeciLM-7B",
     vocab_size=len(tokenizer),
     n_ctx=context_length,
     bos_token_id=tokenizer.bos_token_id,
@@ -74,8 +44,10 @@ config.intermediate_size = intermediate_size
 
 ### Create the llama model with our custom config. Convert it to bitnet.
 # See utils.py for BitLinear and convert_to_bitnet function details.
-model = LlamaForCausalLM(config)                  
+model = DeciLMForCausalLM(config)      
+print("Model created")
 convert_to_bitnet(model, copy_weights=False)
+print("converted to bitnet")
 
 ### Print number of parameters.
 model_size = sum(t.numel() for t in model.parameters())
@@ -89,7 +61,7 @@ data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
 # Adjust the batch size until you can train on your device. Then increase accumulation steps to satisfy the following:
 # tokens per batch = per_device_train_batch_size * gradient_accumulation_steps * 256
 # Adjust this until tokens per batch is at least ~100k.
-output_path = "<folder_to_save_checkpoints>"
+output_path = "outputs"
 args = TrainingArguments(
     output_dir=output_path,
     per_device_train_batch_size=200,
@@ -105,7 +77,7 @@ args = TrainingArguments(
     learning_rate=1.5e-3,
     save_steps=0.25,
     fp16=True,
-    report_to="wandb"
+    report_to="wandb" #  in case you don't want to use wandb it should be report_to="none"
 )
 
 trainer = Trainer(
@@ -117,6 +89,8 @@ trainer = Trainer(
     eval_dataset=tokenized_data["test"],
 )
 
+print("starting training...")
 trainer.train()
 
-trainer.save_model(f"{output_path}/final_model")
+trainer.save_model(f"{output_path}/model")
+print("Model saved successfully!")
